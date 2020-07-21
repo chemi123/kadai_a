2f2886761f1a6fd5bffb06d2298de23dd979c378
Yuichi Tanimoto <>
2020-07-21 17:26:15 +0900

    enable a.go to take dataset for question A

--- a.go
+++ a.go
@@ -4,10 +4,24 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
-	fmt.Println(scanner.Text())
+	// 簡単のために、絶対に正しいデータが来るとしてエラーバリデーションはかけない
+	dataSetNum, _ := strconv.Atoi(strings.TrimSpace(scanner.Text()))
+	fmt.Println(dataSetNum)
+
+	for i := 0; i < dataSetNum; i++ {
+		scanner.Scan()
+		specialLimit, _ := strconv.Atoi(strings.TrimSpace(scanner.Text()))
+		fmt.Println(specialLimit)
+
+		scanner.Scan()
+		enemyList := strings.Split(scanner.Text(), " ")
+		fmt.Println(enemyList)
+	}
 }
