d658036defd2e1445951252aa497be7ce9a6227e
Yuichi Tanimoto <>
2020-07-21 17:28:30 +0900

    delete print debug

--- a.go
+++ a.go
@@ -2,26 +2,26 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"os"
 	"strconv"
 	"strings"
 )
+
+func canDefeatAllEnemies(enemyList []int, specialLimit int) (int, bool) {
+	return 0, false
+}
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 	// 簡単のために、絶対に正しいデータが来るとしてエラーバリデーションはかけない
 	dataSetNum, _ := strconv.Atoi(strings.TrimSpace(scanner.Text()))
-	fmt.Println(dataSetNum)
 
 	for i := 0; i < dataSetNum; i++ {
 		scanner.Scan()
 		specialLimit, _ := strconv.Atoi(strings.TrimSpace(scanner.Text()))
-		fmt.Println(specialLimit)
 
 		scanner.Scan()
 		enemyList := strings.Split(scanner.Text(), " ")
-		fmt.Println(enemyList)
 	}
 }
